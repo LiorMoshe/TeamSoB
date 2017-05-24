b1796800629f1eacc9ba8491aaafd9489d029627
omrigot <gotlib.omri@gmail.com>
2017-05-24 20:07:24 +0300

    Add files via upload

--- ftp/Downloader/FTPDownloader.py
+++ ftp/Downloader/FTPDownloader.py
@@ -2,16 +2,27 @@ import ftplib
 import sys
 import os
 import time
+import random
 
 def downloadFile(ftp, fileName):
 
     ftp.retrbinary('RETR ' + fileName, open(fileName, 'wb').write, 1024)
 	
  
-for i in range(100):
+cur = 0
+while(True):
   ftp = ftplib.FTP('127.0.0.1') #I hope it works.
-  ftp.login('msfadmin','msfadmin')
-# CHANGE DIR?
-  downloadFile(ftp, 'myfile' + str(i))
-  ftp.quit()
-  time.sleep(1)
+  try:
+    for j in range(random.randint(0, 12)):
+      ftp.login('msfadmin','msfadmin')
+      downloadFile(ftp, 'myfile' + str(cur))
+      ftp.delete('myfile' + str(cur))
+      os.remove('myfile' + str(cur))
+      cur = cur + 1
+      time.sleep(random.uniform(0.25,0.3))
+  finally:
+    ftp.quit()
+    time.sleep(random.uniform(0.25,0.3))
+    print cur
+
+
--- ftp/Uploader/FTPUploader.py
+++ ftp/Uploader/FTPUploader.py
@@ -2,21 +2,32 @@ import ftplib
 import sys
 import os
 import time
+import random
 
 def uploadFile(ftp, filename) :
   ftp.storbinary("STOR " + filename, open(filename), 1024)
 
 
+cur = 0
 filename = 'myfile0'
-for i in range(100):
+while(True):
   ftp = ftplib.FTP('127.0.0.1') #I hope it works.
-  ftp.login('msfadmin', 'msfadmin')
-  # CHANGE DIR?
-  os.rename(filename, filename[0:6] + str(i))
-  filename = filename[0:6] + str(i)
-  uploadFile(ftp, filename)
-  ftp.quit()
-  time.sleep(1)
+  try :
+    ftp.login('msfadmin', 'msfadmin')   
+    for j in range(random.randint(0, 12)):
+      os.rename(filename, filename[0:6] + str(cur))
+      filename = filename[0:6] + str(cur)
+      uploadFile(ftp, filename)
+      cur = cur + 1
+      if (cur > 40):
+        os.rename(filename, filename[0:6] + '0')
+        cur = 0
+      time.sleep(random.uniform(0.2,0.25))
+    for j in range(random.randint(0, 1)):
+      ftp.nlst()
+  finally :
+    ftp.quit()
+    time.sleep(random.uniform(0.2, 0.25))
 
 
 
