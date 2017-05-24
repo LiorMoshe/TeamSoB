6cb7637db48254b8a5c7d6d17ef0dfa704367e0e
omrigot <gotlib.omri@gmail.com>
2017-05-24 17:20:33 +0300

    Add files via upload

--- ftp/Downloader/FTPDownloader.py
+++ ftp/Downloader/FTPDownloader.py
@@ -1,6 +1,7 @@
 import ftplib
 import sys
 import os
+import time
 
 def downloadFile(ftp, fileName):
 
@@ -11,6 +12,6 @@ for i in range(100):
   ftp = ftplib.FTP('127.0.0.1') #I hope it works.
   ftp.login('msfadmin','msfadmin')
 # CHANGE DIR?
-  downloadFile(ftp, 'myfile.txt')
+  downloadFile(ftp, 'myfile' + str(i))
   ftp.quit()
-  sleep(1)
+  time.sleep(1)
